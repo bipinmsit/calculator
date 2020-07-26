8970ed236d445a13c1d4c95433dcc8344f2fd0db
bipinmsit <bipin.kumar@aspecscire.com>
2020-07-26 18:39:08 +0530

    Updates

---
+++ precache-manifest.a55fe36baf74d2f15eeab4e4c633b05e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cf2bbed16460005d0e16e5e88d169196",
+    "url": "/calculator/index.html"
+  },
+  {
+    "revision": "5802072e6dd8d40058e0",
+    "url": "/calculator/static/css/main.70482745.chunk.css"
+  },
+  {
+    "revision": "58345dc095a0ba1cee5a",
+    "url": "/calculator/static/js/2.b907d728.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/calculator/static/js/2.b907d728.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5802072e6dd8d40058e0",
+    "url": "/calculator/static/js/main.580cb3fe.chunk.js"
+  },
+  {
+    "revision": "69ac0661a66fb77ed340",
+    "url": "/calculator/static/js/runtime-main.a7a11e39.js"
+  }
+]);
